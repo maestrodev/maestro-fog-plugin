942d7896bcebec7a58c10e501df95baffacac4e6
Carlos Sanchez <csanchez@maestrodev.com>
2013-08-01 12:07:42 +0200

    Update fog to 1.14 snapshot and use new vsphere mocks
    We can use official fog 1.15 when released

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source "https://rubygems.org"
 gem 'maestro_plugin', '>= 0.0.5'
 
 #dependencies
-gem "fog-maestrodev", "~>1.8.0" # we can upgrade to Fog 1.15+ (need git ref 10fa1c4+)
+gem "fog-maestrodev", ">1.14.0" # we can upgrade to Fog 1.15+ (need git ref 10fa1c4+)
 gem "rbvmomi", ">=1.3.0"
 gem "excon", "<0.25.3" # 0.25.3 is broken in JRuby jars https://github.com/geemus/excon/issues/257
 
--- spec/v_sphere_worker_spec.rb
+++ spec/v_sphere_worker_spec.rb
@@ -3,49 +3,6 @@
 require 'spec_helper'
 require 'v_sphere_worker'
 
-# connection.servers output in vSphere 5 test is a bit different than the Fog test data
-#
-# [  <Fog::Compute::Vsphere::Server
-#     id="524de983-d2d5-b55c-5fb7-bcd1e9a9f586",
-#     name="VMware vCenter Server Appliance",
-#     uuid="564db793-79af-3dc2-a012-2615c76cf8c3",
-#     instance_uuid="524de983-d2d5-b55c-5fb7-bcd1e9a9f586",
-#     hostname=nil,
-#     operatingsystem=nil,
-#     ipaddress=nil,
-#     power_state="poweredOff",
-#     tools_state="toolsNotRunning",
-#     tools_version="guestToolsUnmanaged",
-#     mac_addresses={"Network adapter 1"=>"00:0c:29:6c:f8:c3"},
-#     hypervisor="localhost.localdomain",
-#     is_a_template=false,
-#     connection_state="connected",
-#     mo_ref="1",
-#     path="/ha-folder-root/ha-datacenter/vm",
-#     memory_mb=8192,
-#     cpus=2
-#   >,
-#    <Fog::Compute::Vsphere::Server
-#     id="52c96748-e203-669e-e104-b2d7b11179e8",
-#     name="vm-669",
-#     uuid="564dbd30-e464-7acc-f5d1-007aaa95b87a",
-#     instance_uuid="52c96748-e203-669e-e104-b2d7b11179e8",
-#     hostname=nil,
-#     operatingsystem=nil,
-#     ipaddress=nil,
-#     power_state="poweredOn",
-#     tools_state="toolsNotInstalled",
-#     tools_version="guestToolsNotInstalled",
-#     mac_addresses={"Network adapter 1"=>"00:0c:29:95:b8:7a"},
-#     hypervisor="localhost.localdomain",
-#     is_a_template=false,
-#     connection_state="connected",
-#     mo_ref="4",
-#     path="/ha-folder-root/ha-datacenter/vm",
-#     memory_mb=512,
-#     cpus=1
-#   >]
-
 describe MaestroDev::FogPlugin::VSphereWorker, :provider => "vsphere" do
 
   let(:connection) {
@@ -67,6 +24,7 @@ describe MaestroDev::FogPlugin::VSphereWorker, :provider => "vsphere" do
   before do
     subject.workitem = {"fields" => fields}
     subject.stub(:connect => connection)
+    connection.reset_data
   end
 
   describe 'provision' do
@@ -83,42 +41,16 @@ describe MaestroDev::FogPlugin::VSphereWorker, :provider => "vsphere" do
     }}
 
     context 'when provisioning a machine' do
-
-      before do
-        # Fog.Mock is not complete
-        connection.should_receive(:vm_clone).with({
-          "datacenter" => datacenter,
-          "name" => "xxx",
-          "template_path" => template_path,
-          "dest_folder" => destination_folder,
-          "poweron" => true,
-          "wait" => false
-        }).and_return({
-          'vm_ref'   => 'vm-715',
-          'new_vm'   => {'id' => 'vm-715'},
-          'task_ref' => 'task-1234',
-        })
-
-        # vm is returned without ip in mocks, so stub around it
-        vm = connection.get_virtual_machine('vm-715')
-        vm[:public_ip_address] = vm[:ipaddress]
-        connection.stub(:get_virtual_machine).with('5032c8a5-9c5e-ba7a-3804-832a03e16381').and_call_original
-        connection.stub(:get_virtual_machine).with('5032c8a5-9c5e-ba7a-3804-832a03e16381', nil).and_call_original
-        connection.should_receive(:get_virtual_machine).with('vm-715', nil).and_return(vm)
-
-        subject.stub(:workitem => {"fields" => fields}, :connect => connection)
-        subject.provision
-      end
-
+      before { subject.provision }
       its(:error) { should be_nil }
-      it { expect(subject.workitem[Maestro::MaestroWorker::OUTPUT_META]).to include(
-        "Server 'jefftest' 5032c8a5-9c5e-ba7a-3804-832a03e16381 started with public ip '192.168.100.187' and private ip ''") }
-      it { expect(field('vsphere_ids')).to eq(["5032c8a5-9c5e-ba7a-3804-832a03e16381"]) }
+      it { expect(subject.workitem[Maestro::MaestroWorker::OUTPUT_META]).to match(
+        /Server 'xxx' #{uuid_regex} started with public ip '192.168.100.184' and private ip ''/) }
+      it { expect(field('vsphere_ids').to_s).to match(/^\["#{uuid_regex}"\]$/) }
     end
 
     context 'when template does not exist' do
-      let(:fields) { super.merge({"template_path" => "doesnotexist"}) }
       before { subject.provision }
+      let(:fields) { super.merge({"template_path" => "doesnotexist"}) }
       its(:error) { should eq("VM template 'doesnotexist': Could not find VM template") }
     end
 
@@ -192,4 +124,9 @@ describe MaestroDev::FogPlugin::VSphereWorker, :provider => "vsphere" do
       its(:error) { should be_nil }
     end
   end
+
+  def uuid_regex
+    "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"
+  end
+
 end
